9cbe01c82c756de8a9daba14216c94845e4c7676
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2013-12-20 18:57:48 -0500

    Added some numeric helper functions

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 exports.Parser = require("./parser");
 exports.Serialiser = require("./serialiser");
-exports.Numerics = require("./numerics");
+
+var numerics = require("./numerics");
+
+function numericToName(number) {
+	return numerics[''+number];
+}
+
+var names = Object.keys(numerics).reduce(function (obj, key) {
+	obj[numerics[key]] = key;
+
+	return obj;
+}, {});
+
+function nameToNumeric(name) {
+	return names[name];
+}
+
+exports.Numerics = {
+	toName: numericToName,
+	fromName: nameToNumeric
+};
